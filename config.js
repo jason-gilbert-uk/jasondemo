2d9cfe399ae9f34a6ffdc9fdd6b3b0366caaf90b
Jason Gilbert <jasongilbertuk@refinitiv.com>
2020-11-28 13:36:29 +0000

    latest

--- config.js
+++ config.js
@@ -23,6 +23,6 @@ exports.googleanalytics = {
 exports.database = {
 	type: 'redis',
 	prefix: '#scrumblr#',
-	redis: argv.redis || 'redis://127.0.0.1:6379'
+	redis: argv.redis || 'redis://redis.example.com:6379'
 };
 
